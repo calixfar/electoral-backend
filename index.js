747a083753cc525e70fc71dccfa6bc37bf3cc0b1
Luis Alexander Santiago Espinosa <luis-santiago97@hotmail.com>
2019-07-31 17:59:32 -0500

    Primer commit backend

---
+++ index.js
@@ -0,0 +1,33 @@
+import {resolvers} from './data.js/resolvers'
+import {ApolloServer} from 'apollo-server-express';
+import {typeDefs} from './data.js/schema'
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config({path: 'variables.env'})
+import jwt from 'jsonwebtoken';
+const app = express();
+
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers,
+    context : async ({req}) => {
+        //obtener token
+        const token = req.headers['authorization'];
+        if(token !== 'null'){
+            try {
+                //verificar el token del cliente
+                const usuarioActual = await jwt.verify(token, process.env.SECRETO)
+                req.usuarioActual = usuarioActual;
+                return {usuarioActual}
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+
+    });
+
+server.applyMiddleware({app})
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+app.listen({port},{host}, () => console.log(`Servidor corriendo en http://localhost:4000${server.graphqlPath}`))
